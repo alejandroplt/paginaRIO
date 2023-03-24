52b78945b76ad380790722692b94378bf7ca0ae7
alejandroplt <alejandro.platas.02@gmail.com>
2023-03-24 00:39:27 -0600

    FUNCIONES DEL SCROLL

--- js/index.js
+++ js/index.js
@@ -9,3 +9,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+window.addEventListener('scroll', function()  {
+    let elements = document.getElementsByClassName('scroll-content');
+    let screenSize = window.innerHeight;
+    
+          for(var i = 0; i < elements.length; i++) {
+          var element = elements[i];
+  
+          if(element.getBoundingClientRect().top < screenSize) {
+            element.classList.add('visible');
+          } else {
+            element.classList.remove('visible');
+          }
+  
+        }
+  });
