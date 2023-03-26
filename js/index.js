3623d4c8b72d9253e78ae46e2964701d1195476b
alejandroplt <alejandro.platas.02@gmail.com>
2023-03-26 00:07:38 -0600

    SECCIONES SCROLL

--- js/index.js
+++ js/index.js
@@ -9,20 +9,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-
-
-window.addEventListener('scroll', function()  {
-    let elements = document.getElementsByClassName('scroll-content');
-    let screenSize = window.innerHeight;
-    
-          for(var i = 0; i < elements.length; i++) {
-          var element = elements[i];
-  
-          if(element.getBoundingClientRect().top < screenSize) {
-            element.classList.add('visible');
-          } else {
-            element.classList.remove('visible');
-          }
-  
-        }
-  });
