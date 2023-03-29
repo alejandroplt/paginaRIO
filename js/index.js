ca32d338c11a7090d415d436bb9faf2a0a029625
alejandroplt <alejandro.platas.02@gmail.com>
2023-03-28 21:19:37 -0600

    PRUEBA BOTON SUBIR ARRIBA

--- js/index.js
+++ js/index.js
@@ -8,3 +8,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 100) {
+      document.querySelector('.go-top-container').classList.add('show');
+       
+    }
+    else{
+      document.querySelector('.go-top-container').classList.remove('show');
+    }
+  }
+   
+  document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
